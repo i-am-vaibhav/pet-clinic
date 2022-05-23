aee4c90412ce123052c7a8d7ea5cc1a99d1b2559
i-am-vaibhav <vg566556@gmail.com>
2022-05-24 03:24:20 +0530

    closes #6 added index page and controller

---
+++ pet-clinic-web/src/main/java/com/pet/clinic/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.pet.clinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"","/","index.html","index"})
+    public String index(Model model){
+        model.addAttribute("msg","Hello guys");
+        return "index";
+    }
+
+}
