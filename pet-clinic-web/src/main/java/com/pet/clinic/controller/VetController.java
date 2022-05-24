c8600fd9257deec2390a76b667ec3221233cff4d
i-am-vaibhav <vg566556@gmail.com>
2022-05-25 01:32:49 +0530

    closes #11 added vets list in the page

--- pet-clinic-web/src/main/java/com/pet/clinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/pet/clinic/controller/VetController.java
@@ -1,5 +1,6 @@
 package com.pet.clinic.controller;
 
+import com.pet.clinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 
