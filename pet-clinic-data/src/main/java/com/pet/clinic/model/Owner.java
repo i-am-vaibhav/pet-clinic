198bd13f1a9985bd319c53aa7bd0aacb951dd6cb
i-am-vaibhav <vg566556@gmail.com>
2022-05-31 17:19:29 +0530

    closes #21 added contact info

--- pet-clinic-data/src/main/java/com/pet/clinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/pet/clinic/model/Owner.java
@@ -4,13 +4,43 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+
+    private String city;
+
+    private String mobile;
     private Set<Pet> pets;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 
     public Set<Pet> getPets() {
         return pets;
     }
+    public String getCity() {
+        return city;
+    }
 
-    public void setPet(Set<Pet> pets) {
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
+
 }
