30456d7493906da8c2ce74e9fcf781da28e97fcf
i-am-vaibhav <vg566556@gmail.com>
2022-05-31 17:07:58 +0530

    closes #19 added visit

--- pet-clinic-data/src/main/java/com/pet/clinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/pet/clinic/model/Owner.java
@@ -1,6 +1,16 @@
 package com.pet.clinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPet(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
