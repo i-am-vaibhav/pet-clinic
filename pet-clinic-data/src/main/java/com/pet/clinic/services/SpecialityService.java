c86adf5ef1eb8fca99a123c77256244ad7fce475
i-am-vaibhav <vg566556@gmail.com>
2022-06-01 19:22:22 +0530

    closes #24 add the specialityService

---
+++ pet-clinic-data/src/main/java/com/pet/clinic/services/SpecialityService.java
@@ -0,0 +1,8 @@
+package com.pet.clinic.services;
+
+import com.pet.clinic.model.Speciality;
+import com.pet.clinic.model.Vet;
+
+public interface SpecialityService extends BaseService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/pet/clinic/services/map/SpecialityMapService.java
@@ -0,0 +1,10 @@
+package com.pet.clinic.services.map;
+
+import com.pet.clinic.model.Speciality;
+import com.pet.clinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialityService {
+
+}
