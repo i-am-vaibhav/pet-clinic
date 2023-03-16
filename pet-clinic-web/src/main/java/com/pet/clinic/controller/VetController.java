e7b4d9f84901baae707511d67e600b692ec2911f
i-am-vaibhav <vg566556@gmail.com>
2023-03-16 20:18:44 +0530

    Closes #49, added the vets JSON endpoint

--- pet-clinic-web/src/main/java/com/pet/clinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/pet/clinic/controller/VetController.java
@@ -1,15 +1,20 @@
 package com.pet.clinic.controller;
 
+import com.pet.clinic.model.Vet;
 import com.pet.clinic.services.VetService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Slf4j
 @Controller
-@RequestMapping("/vets")
+@RequestMapping
 public class VetController {
 
     private final VetService vetService;
@@ -23,6 +28,12 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         log.debug("list of vets");
         return "vets/index";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public ResponseEntity<Set<Vet>> getVets(){
+        return ResponseEntity.ok(vetService.findAll());
     }
 
 }
